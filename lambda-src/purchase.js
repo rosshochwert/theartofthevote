e7179fea9c640bdd1fe878ad0318e1d648b092ae
Ross Hochwert <rosshochwert@gmail.com>
2020-08-13 23:07:01 -0400

    new checkout page

--- lambda-src/purchase.js
+++ lambda-src/purchase.js
@@ -14,66 +14,22 @@ exports.handler = async function(event) {
       statusCode: 200, // <-- Important!
       headers,
       body: "This was not a POST request!"
-  };
-}
+    };
+  }
 
-  // Parse the body contents into an object.
   const data = JSON.parse(event.body);
 
-  // Make sure we have all required data. Otherwise, get outta here.
-  if (!data.token || !data.amount || !data.idempotency_key) {
-  	const message = "Required information is missing!";
-
-  	console.error(message);
-
-  	return {
-  		statusCode,
-  		headers,
-  		body: JSON.stringify({
-  			status: "failed",
-  			message
-  		})
-  	};
-  }
-
-  let charge;
-
-  try {
-  	charge = await stripe.charges.create(
-  	{
-  		currency: "usd",
-  		amount: data.amount,
-  		source: data.token.id,
-  		receipt_email: data.token.email,
-  		description: `charge for a widget`
-  	},
-  	{
-  		idempotency_key: data.idempotency_key
-  	}
-  	);
-  } catch (e) {
-  	let message = e.message;
-
-  	console.error(message);
-
-  	return {
-  		statusCode: 424,
-  		headers,
-  		body: JSON.stringify({
-  			status: "failed",
-  			message
-  		})
-  	};
-  }
-
-  const status = (charge === null || charge.status !== "succeeded") ? "failed" : charge.status;
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: calculateOrderAmount(data),
+    currency: "usd"
+  });
 
   return {
-  	statusCode,
-  	headers,
-  	body: JSON.stringify({
-  		status,
-  		message: "Charge successfully created!"
-  	})
-  };
+    statusCode: 200, // <-- Important!
+      headers,
+      body: {
+        clientSecret: paymentIntent.client_secret
+      }
+  }
+
 };
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,41 +1,101 @@
-const { v4: uuidv4 } = require('uuid');
+var stripe = Stripe("pk_test_51HFo77FL2039H5Ri4ovbl2tOvdsa1yhxoVRcJozYX1rsRv2KmYhSvgyNXvAO57CMw7QTxANJarZGTqpeNbscXCeW00DgsNrtjq");
 
-const handler = StripeCheckout.configure({
-	key: STRIPE_PUBLISHABLE_KEY,
-	image: "https://stripe.com/img/documentation/checkout/marketplace.png",
-	locale: "auto",
-	token: async (token) => {
-		let response, data;
+var payWithCard = function(stripe, card, clientSecret) {
+  loading(true);
+  stripe
+    .confirmCardPayment(clientSecret, {
+      payment_method: {
+        card: card
+      }
+    })
+    .then(function(result) {
+      if (result.error) {
+        // Show error to your customer
+        showError(result.error.message);
+      } else {
+        // The payment succeeded!
+        orderComplete(result.paymentIntent.id);
+      }
+    });
+};
 
-		try {
-			response = await fetch(LAMBDA_ENDPOINT, {
-				method: "POST",
-				body: JSON.stringify({
-					token,
-					amount,
-					idempotency_key: uuid4()
-				}),
-				headers: new Headers({
-					"Content-Type": "application/json"
-				})
-			});
+var orderComplete = function(paymentIntentId) {
+  loading(false);
+  document
+    .querySelector(".result-message a")
+    .setAttribute(
+      "href",
+      "https://dashboard.stripe.com/test/payments/" + paymentIntentId
+    );
+  document.querySelector(".result-message").classList.remove("hidden");
+  document.querySelector("button").disabled = true;
+};
 
-			data = await response.json();
-		} catch (error) {
-			console.error(error.message);
-			return;
-		}
-	}
-});
-
-const amount = 1000;
+var showError = function(errorMsgText) {
+  loading(false);
+  var errorMsg = document.querySelector("#card-error");
+  errorMsg.textContent = errorMsgText;
+  setTimeout(function() {
+    errorMsg.textContent = "";
+  }, 4000);
+};
+// Show a spinner on payment submission
+var loading = function(isLoading) {
+  if (isLoading) {
+    // Disable the button and show a spinner
+    document.querySelector("button").disabled = true;
+    document.querySelector("#spinner").classList.remove("hidden");
+    document.querySelector("#button-text").classList.add("hidden");
+  } else {
+    document.querySelector("button").disabled = false;
+    document.querySelector("#spinner").classList.add("hidden");
+    document.querySelector("#button-text").classList.remove("hidden");
+  }
+};
 
 document.addEventListener("DOMContentLoaded", function(){
-	document.querySelector("button").addEventListener("click", function() {
-		handler.open({
-			amount,
-			name: "Test Shop",
-			description: "A Fantastic New Widget"
-		});
-	});
+	document.querySelector("button").disabled = true;
+	fetch("/purchase", {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json"
+		},
+		body: JSON.stringify(purchase)
+	})
+	.then(function(result) {
+		return result.json();
+	})
+	.then(function(data) {
+		var elements = stripe.elements();
+		var style = {
+			base: {
+				color: "#32325d",
+				fontFamily: 'Arial, sans-serif',
+				fontSmoothing: "antialiased",
+				fontSize: "16px",
+				"::placeholder": {
+					color: "#32325d"
+				}
+			},
+			invalid: {
+				fontFamily: 'Arial, sans-serif',
+				color: "#fa755a",
+				iconColor: "#fa755a"
+			}
+		};
+		var card = elements.create("card", { style: style });
+    // Stripe injects an iframe into the DOM
+    card.mount("#card-element");
+    card.on("change", function (event) {
+      // Disable the Pay button if there are no card details in the Element
+      document.querySelector("button").disabled = event.empty;
+      document.querySelector("#card-error").textContent = event.error ? event.error.message : "";
+  	});
+    var form = document.getElementById("payment-form");
+    form.addEventListener("submit", function(event) {
+    	event.preventDefault();
+      // Complete payment when the submit button is clicked
+      	payWithCard(stripe, card, data.clientSecret);
+    });
+
 });
