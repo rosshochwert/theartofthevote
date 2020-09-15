f58d475529d085514479b9b0ceb0b9cdc5118902
Ross Hochwert <rosshochwert@gmail.com>
2020-09-14 21:30:23 -0400

    ross

--- src/js/donateScript.js
+++ src/js/donateScript.js
@@ -18,39 +18,39 @@ var payWithCard = function(stripe, card, clientSecret) {
 };
 
 var orderComplete = function(paymentIntentId, donation) {
-	sendToNetlify(donation)
+    sendToNetlify(donation)
 };
 
-var sendToNetlify = function(donation){
-	var $form = $("#payment-form");
-	var form_data = new FormData($('#payment-form')[0]);
-/*	$.post($form.attr("action"), $form.serialize()).then(function() {
-    	alert("Thank you!");
-  	});*/
+var sendToNetlify = function(donation) {
+    var $form = $("#payment-form");
+    var form_data = new FormData($('#payment-form')[0]);
+    /*  $.post($form.attr("action"), $form.serialize()).then(function() {
+            alert("Thank you!");
+        });*/
 
-	$.ajax({
-	    url: $form.attr("action"),
-	    type: 'post',
-	    data: form_data,
-	    contentType: false,
-	    //contentType: 'multipart/form-data',
+    $.ajax({
+        url: $form.attr("action"),
+        type: 'post',
+        data: form_data,
+        contentType: false,
+        //contentType: 'multipart/form-data',
         processData: false,
-	    success: function (data) {
-	        if (donation){
-	        	document.querySelector(".result-donate-message").classList.remove("hidden");
-	        } else {
-	        	document.querySelector(".result-no-donate-message").classList.remove("hidden");
-	        }
-	    	document.querySelector("#submit").disabled = true;
-	    	loading(false);
-	    	sendEmail(donation)
+        success: function(data) {
+            if (donation) {
+                document.querySelector(".result-donate-message").classList.remove("hidden");
+            } else {
+                document.querySelector(".result-no-donate-message").classList.remove("hidden");
+            }
+            document.querySelector("#submit").disabled = true;
+            loading(false);
+            sendEmail(donation)
 
-	    }, 
-	    error: function (request, status, error) {
-	    	console.log(request)
-	    	console.log(status)
-	    }
-	});
+        },
+        error: function(request, status, error) {
+            console.log(request)
+            console.log(status)
+        }
+    });
 
 };
 
@@ -80,8 +80,8 @@ var loading = function(isLoading) {
 
 var sendEmail = function(donation) {
 
-	var nameValue = document.getElementById("name").value;
-	var emailValue = document.getElementById("email").value;
+    var nameValue = document.getElementById("name").value;
+    var emailValue = document.getElementById("email").value;
 
     fetch("/.netlify/functions/sendmail", {
             method: "POST",
@@ -89,9 +89,9 @@ var sendEmail = function(donation) {
                 "Content-Type": "application/json"
             },
             body: JSON.stringify({
-            	name: nameValue,
-            	email: emailValue,
-            	donation: donation
+                name: nameValue,
+                email: emailValue,
+                donation: donation
             }) //need to define how much to purchase first duh
         })
         .then(function(result) {
@@ -114,89 +114,116 @@ function triggerBrowserValidation() {
     submit.remove();
 }
 
-document.addEventListener("DOMContentLoaded", function() {
-	var stripe = Stripe("pk_test_51HFo77FL2039H5Ri4ovbl2tOvdsa1yhxoVRcJozYX1rsRv2KmYhSvgyNXvAO57CMw7QTxANJarZGTqpeNbscXCeW00DgsNrtjq", {
-		maxNetworkRetries: 2
-	});
-
-/*	document.getElementById("donateButton").addEventListener("click", function(){
-		document.getElementById("card-element").style.display = "block"
-		document.querySelector("#submit").disabled = true;
-	})*/
-
-/*    document.querySelector("#submit").disabled = true;
-*/
-    
+function fetchPaymentIntent(amount) {
+    console.log("fetching payment")
     fetch("/.netlify/functions/purchase", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
             },
-            body: 100 //need to define how much to purchase first duh
+            body: amount //need to define how much to purchase first duh
         })
         .then(function(result) {
             return result.json();
         })
-        .then(function(data) {
-            var elements = stripe.elements({
-                fonts: [{
-                    cssSrc: 'https://fonts.googleapis.com/css?family=Roboto'
-                }]
-            });
-            var style = {
-                base: {
-                    color: "#32325d",
-                    fontFamily: 'Arial, sans-serif',
-                    fontSmoothing: "antialiased",
-                    fontSize: "16px",
-                    "::placeholder": {
-                        color: "#32325d"
-                    }
-                },
-                invalid: {
-                    fontFamily: 'Arial, sans-serif',
-                    color: "#fa755a",
-                    iconColor: "#fa755a"
-                }
-            };
-            var card = elements.create("card", {
-                style: style
-            });
-            // Stripe injects an iframe into the DOM
-            card.mount("#card-element");
-            card.on("change", function(event) {
-                // Disable the Pay button if there are no card details in the Element
-                document.querySelector("#submit").disabled = event.empty;
-                document.querySelector("#card-error").textContent = event.error ? event.error.message : "";
-            });
+}
 
-            var form = document.getElementById("payment-form");
+var card;
+function mountForm() {
+    var elements = stripe.elements({
+        fonts: [{
+            cssSrc: 'https://fonts.googleapis.com/css?family=Roboto'
+        }]
+    });
+    var style = {
+        base: {
+            color: "#32325d",
+            fontFamily: 'Arial, sans-serif',
+            fontSmoothing: "antialiased",
+            fontSize: "16px",
+            "::placeholder": {
+                color: "#32325d"
+            }
+        },
+        invalid: {
+            fontFamily: 'Arial, sans-serif',
+            color: "#fa755a",
+            iconColor: "#fa755a"
+        }
+    };
+    card = elements.create("card", {
+        style: style
+    });
+    // Stripe injects an iframe into the DOM
+    card.mount("#card-element");
+    card.on("change", function(event) {
+        // Disable the Pay button if there are no card details in the Element
+        document.querySelector("#submit").disabled = event.empty;
+        document.querySelector("#card-error").textContent = event.error ? event.error.message : "";
+    });
+}
 
-            form.addEventListener("submit", function(event) {
-                event.preventDefault();
-                // Trigger HTML5 validation UI on the form if any of the inputs fail
-                // validation.
-                var plainInputsValid = true;
-                Array.prototype.forEach.call(form.querySelectorAll('input'), function(
-                    input
-                ) {
-                    if (input.checkValidity && !input.checkValidity()) {
-                        plainInputsValid = false;
-                        return;
-                    }
-                });
 
-                if (!plainInputsValid) {
-                    triggerBrowserValidation();
-                    return;
-                } else if (document.getElementById("card-element").style.display == "block") {
-                    payWithCard(stripe, card, data.clientSecret);
-                } else if (document.getElementById("card-element").style.display == "none") {
-                	sendToNetlify(false)
-                }
+var stripe;
 
-                // Complete payment when the submit button is clicked
-            });
+document.addEventListener("DOMContentLoaded", function() {
+    stripe = Stripe("pk_test_51HFo77FL2039H5Ri4ovbl2tOvdsa1yhxoVRcJozYX1rsRv2KmYhSvgyNXvAO57CMw7QTxANJarZGTqpeNbscXCeW00DgsNrtjq", {
+        maxNetworkRetries: 2
+    });
+
+    mountForm()
+
+
+    document.getElementById("submit").addEventListener("click", function() {
+        event.preventDefault();
+        document.getElementById("card-element").style.display = "block"
+        document.querySelector("#submit").disabled = true;
+        var plainInputsValid = true;
+        var form = document.getElementById("payment-form");
+        Array.prototype.forEach.call(form.querySelectorAll('input'), function(
+            input
+        ) {
+            if (input.checkValidity && !input.checkValidity()) {
+                plainInputsValid = false;
+                return;
+            }
         });
+        if (plainInputsValid){
+            paymentId = new Promise(fetchPaymentIntent(100))
+            paymentId.then((data) => { 
+                payWithCard(stripe, card, data.clientSecret);
+            });
+        } else {
+            triggerBrowserValidation();
+        }
+    })
+
+    document.getElementById("fiveDollar").addEventListener("click", function() {
+        document.getElementById("fiveDollar").style.background = "blue"
+        document.getElementById("tenDollar").style.background = "none"
+        document.getElementById("inputDollar").value = "5.00"
+    })
+
+    document.getElementById("tenDollar").addEventListener("click", function() {
+        document.getElementById("tenDollar").style.background = "blue"
+        document.getElementById("fiveDollar").style.background = "none"
+        document.getElementById("inputDollar").value = "10.00"
+    })
+
+    document.getElementById("inputDollar").addEventListener('change', (event) => {
+        if (event.target.value == 5) {
+            document.getElementById("fiveDollar").style.background = "blue"
+            document.getElementById("tenDollar").style.background = "none"
+        } else if (event.target.value == 10) {
+            document.getElementById("tenDollar").style.background = "blue"
+            document.getElementById("fiveDollar").style.background = "none"
+        } else {
+            document.getElementById("fiveDollar").style.background = "none"
+            document.getElementById("tenDollar").style.background = "none"
+        }
+    });
+
+
+    document.querySelector("#submit").disabled = true;
 
 });
